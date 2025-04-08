afe23239f8ab26a9c595d0aa73bf9b30478536c1
fakcior <fakcior00@gmail.com>
2025-04-08 22:30:36 +0200

    Nozzle size display; Pressure advance override; filament options override; Gcode commands refactor

--- src/components/mixins/toolchanger.ts
+++ src/components/mixins/toolchanger.ts
@@ -31,6 +31,10 @@ export default class ToolchangerMixin extends Vue {
         return this.$store.state.printer.save_variables?.variables?.filament_types
     }
 
+    get toolchangerFilamentTypesOptions(): string[] {
+        return this.$store.state.printer.save_variables?.variables?.filament_types_options ?? ['PLA', 'ABS', 'ASA', 'PETG', 'PA', 'PC', 'TPU']
+    }
+
     get toolchangerFilamentColors(): string[] {
         return this.$store.state.printer.save_variables?.variables?.filament_colors
     }
@@ -42,25 +46,5 @@ export default class ToolchangerMixin extends Vue {
     doSend(gcode: string) {
         this.$store.dispatch('server/addEvent', { message: gcode, type: 'command' })
         this.$socket.emit('printer.gcode.script', { script: gcode })
-    }
-
-    doSetToolColor(tool: number, color: string) {
-        let colors = this.toolchangerFilamentColors
-        if (!colors || colors.length != this.toolchangerToolNames.length) {
-            colors = new Array(this.toolchangerToolNames.length).fill('#FFFFFF')
-        }
-        colors[tool] = color
-        const gcode2 = `SAVE_VARIABLE VARIABLE=filament_colors VALUE='${JSON.stringify(colors)}'`
-        this.doSend(gcode2)
-    }
-
-    doSetToolMaterial(tool: number, material: string) {
-        let materials = this.toolchangerFilamentTypes
-        if (!materials || materials.length != this.toolchangerToolNames.length) {
-            materials = new Array(this.toolchangerToolNames.length).fill('PLA')
-        }
-        materials[tool] = material
-        const gcode2 = `SAVE_VARIABLE VARIABLE=filament_types VALUE='${JSON.stringify(materials)}'`
-        this.doSend(gcode2)
     }
 }
--- src/components/panels/Toolchanger/ToolchangerPanelToolsItem.vue
+++ src/components/panels/Toolchanger/ToolchangerPanelToolsItem.vue
@@ -1,28 +1,34 @@
 <template>
     <div :class="['flex-grow-1 toolchanger-tool', { active: isActive, changing: isChanging && isActive }]">
-        <div class="tool-name">{{ name }}</div>
+        <div class="tool-name">{{ name.toUpperCase() }}</div>
+        <div class="nozzle-info">
+            <v-icon small>{{ mdiPrinter3dNozzle }}</v-icon>
+            <span>{{ nozzleDiameter }} mm</span>
+        </div>
         <div class="filament-info">
             <spool-icon style="margin: 10%;" :color="selectedColor" @click-spool="showColorPicker = true"></spool-icon>
             <v-menu v-model="showColorPicker" :close-on-content-click="false" offset-y>
                 <template #activator="{ on, attrs }">
                     <div v-bind="attrs" v-on="on"></div>
                 </template>
-                <v-color-picker v-model="selectedColor" @input="setToolColor"></v-color-picker>
+                <v-color-picker v-model="selectedColor" @input="(value) => doSend(`SET_TOOL_COLOR TOOL=${toolNumber} COLOR='${value}'`)" ></v-color-picker>
             </v-menu>
-            <v-select style="margin-left: 10%; margin-right: 10%;"  :items="filamentTypes" v-model="selectedType" @change="setToolMaterial" label="Filament Type"></v-select>
+            <v-select style="margin-left: 10%; margin-right: 10%;"  :items="filamentTypes" v-model="selectedType" @change="(value) => doSend(`SET_TOOL_MATERIAL TOOL=${toolNumber} MATERIAL=${value}`)"  label="Filament Type"></v-select>
+            <v-text-field style="margin-left: 10%; margin-right: 10%;" v-model="pressureAdvance" @input="(value) => doSend(`SET_PRESSURE_ADVANCE TOOL=${toolNumber} VALUE=${value}`)" label="Pressure Advance" type="number" step="0.01"></v-text-field>
         </div>
+        
         <div class="tool-buttons">
             <v-btn small @click="doSend(`LOAD_ONE_FILAMENT TOOL=${toolNumber}`)">
                 <v-icon left small>{{mdiRedoVariant}}</v-icon>
-                {{ $t('Load', { isDefault: '' }) }}
+                {{ $t('LOAD', { isDefault: '' }) }}
             </v-btn>
             <v-btn small @click="doSend(`UNLOAD_ONE_FILAMENT TOOL=${toolNumber}`)">
                 <v-icon left small>{{mdiUndoVariant}}</v-icon>
-                {{ $t('Unload', { isDefault: '' }) }}
+                {{ $t('UNLOAD', { isDefault: '' }) }}
             </v-btn>
             <v-btn small @click="doSend(`T${toolNumber}`)">
                 <v-icon left small>{{mdiTargetVariant}}</v-icon>
-                {{ $t('Select', { isDefault: '' }) }}
+                {{ $t('ACTIVATE', { isDefault: '' }) }}
             </v-btn>
         </div>
     </div>
@@ -33,23 +39,28 @@
 import { Component, Mixins, Prop } from 'vue-property-decorator'
 import BaseMixin from '@/components/mixins/base'
 import ToolchangerMixin from '@/components/mixins/toolchanger'
-import { mdiUndoVariant, mdiRedoVariant, mdiTargetVariant } from '@mdi/js'
+import ExtruderMixin from '@/components/mixins/extruder'
+import { mdiUndoVariant, mdiRedoVariant, mdiTargetVariant, mdiPrinter3dNozzle } from '@mdi/js'
 
 @Component
-export default class ToolchangerPanel extends Mixins(BaseMixin, ToolchangerMixin) {
+export default class ToolchangerPanel extends Mixins(BaseMixin, ExtruderMixin, ToolchangerMixin) {
     @Prop(String) name!: string
     mdiUndoVariant = mdiUndoVariant
     mdiRedoVariant = mdiRedoVariant
     mdiTargetVariant = mdiTargetVariant
+    mdiPrinter3dNozzle = mdiPrinter3dNozzle
     showColorPicker = false
-    filamentTypes = ['PLA', 'ABS', 'TPU', 'PETG']
     
+    get filamentTypes(): string[] {
+        return this.toolchangerFilamentTypesOptions
+    }
+
     get selectedColor(): string {
-        return this.toolchangerFilamentColors[Number(this.toolNumber)]
+        return this.toolchangerFilamentColors[this.toolNumber]
     }
 
     get selectedType(): string {
-        return this.toolchangerFilamentTypes[Number(this.toolNumber)]
+        return this.toolchangerFilamentTypes[this.toolNumber]
     }
     get isActive(): boolean {
         return this.toolchangerTool === this.name
@@ -59,16 +70,25 @@ export default class ToolchangerPanel extends Mixins(BaseMixin, ToolchangerMixin
         return this.toolchangerStatus === 'changing'
     }
 
-    get toolNumber(): string {
-        return `${this.name.replace(/\D/g, '')}`
+    get toolNumber(): number {
+        return Number(`${this.name.replace(/\D/g, '')}`)
     }
 
-    setToolColor(color: string) {
-        this.doSetToolColor(Number(this.toolNumber), color);
+    get tCommand(): string {
+        return `T${this.toolNumber}`
     }
 
-    setToolMaterial(material: string) {
-        this.doSetToolMaterial(Number(this.toolNumber), material)
+    get nozzleDiameter(): number {
+        return this.extruders[this.toolNumber].nozzleDiameter
+    }
+
+    get pressureAdvance(): number {
+        if (this.$store.state.printer?.[`gcode_macro ${this.tCommand}`]?.pressure_advance != null) {
+            return this.$store.state.printer?.[`gcode_macro ${this.tCommand}`]?.pressure_advance
+        }
+        else {
+            return this.$store.state.printer?.[this.extruders[this.toolNumber].key]?.pressure_advance ?? 0
+        }
     }
 
 }
@@ -109,7 +129,12 @@ export default class ToolchangerPanel extends Mixins(BaseMixin, ToolchangerMixin
 
 .tool-name {
     font-weight: bold;
-    margin-bottom: 10px;
+}
+
+.nozzle-info {
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 
 .filament-info {
@@ -117,7 +142,6 @@ export default class ToolchangerPanel extends Mixins(BaseMixin, ToolchangerMixin
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    margin-bottom: 10px;
 }
 
 .tool-buttons {
